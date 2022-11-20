8517d7c9bf430df6b410e06591bdec38bcbdbb7e
evikorte <111706114+evikorte@users.noreply.github.com>
2022-11-20 17:32:08 +0100

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by
+        <a
+          href="https://github.com/evikorte/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          evikorte
+        </a>
+      </footer>
+    </div>
+  );
 }
