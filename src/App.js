34c9a997e1549045c83adb5179f52e9bebf3d0f8
evikorte <111706114+evikorte@users.noreply.github.com>
2022-11-20 17:52:05 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by
-        <a
-          href="https://github.com/evikorte/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          evikorte
-        </a>
-      </footer>
+    <div className="App text-center">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by
+          <a
+            href="https://github.com/evikorte/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            evikorte
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
